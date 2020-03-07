12ccbb61aeb948e3ca701ed8cd9c8f404e9bbfb0
dershov <dmitriy.ershov@iqoption.com>
2020-03-08 02:33:18 +0300

    hw1 homework

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	fmt.Println("current time: ", time.Now())
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Println("Failed to get time, exit", err)
+		os.Exit(1)
+	}
+	fmt.Println("exact time: ", ntpTime)
 }
