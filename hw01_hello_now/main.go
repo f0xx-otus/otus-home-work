a2bb161416326f4c85219103df221d4b43e17cce
dershov <dmitriy.ershov@iqoption.com>
2020-03-15 02:53:15 +0300

    First iteration

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -5,13 +5,10 @@ import (
 	"log"
 	"time"
 
-	"github.com/f0xx-otus/otus-home-work/hw02_unpack_string"
-
 	"github.com/beevik/ntp"
 )
 
 func main() {
-	fmt.Println(hw02_unpack_string.Unpack("a4bc2d5e"))
 	fmt.Println("current time:", time.Now())
 	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,17 +2,44 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
-	"fmt"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
-	fmt.Println(input)
 	var output string
-	if output == "" {
-		return "invalid string", ErrInvalidString
+	runeSlice := []rune(input)
+
+	if input == "" {
+		return "", nil
 	}
 
+	for i, r := range runeSlice {
+		if unicode.IsLetter(r) {
+			output += string(r)
+			if i < (len(runeSlice) - 1) {
+				if unicode.IsDigit(runeSlice[i+1]) {
+					output += MultiplyLetters(runeSlice, i)
+				}
+			}
+		}
+		if unicode.IsDigit(r) {
+			if i < (len(runeSlice) - 1) {
+				if unicode.IsDigit(runeSlice[i+1]) {
+					return "", ErrInvalidString
+				}
+			}
+		}
+	}
 	return output, nil
+}
+
+func MultiplyLetters(runeSlice []rune, i int) string {
+	letter := string(runeSlice[i])
+	var multiLetter string
+	for j := 1; j < int(runeSlice[i+1]-'0'); j++ {
+		multiLetter += letter
+	}
+	return multiLetter
 }
